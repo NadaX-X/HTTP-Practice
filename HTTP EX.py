5e12aa7bb4e5de690e50170ab735273667d7711a
NadaX-X <150373566+NadaX-X@users.noreply.github.com>
2024-08-02 20:42:01 +0300

    Update HTTP EX.py

--- HTTP EX.py
+++ HTTP EX.py
@@ -1,48 +1,44 @@
 import requests
 
 def test_1():
-url = "[https://www.google.com](https://www.google.com/)"
-response = requests.get(url)
-print(response.status_code)
-return response
+    url = "https://httpbin.org/get"
+    response = requests.get(url)
+    print(response.status_code)
+    return response
 
 def test_2():
-params ={"q": "python language"}
-response = requests.get("https://www.google.com/search",
-params=params)
-return response
-def tes_3():
-data = {
-"name": "John",
-"age": 30,
-"city": "New York"
-}
-response = requests.post("https://www.google.com/search",
-json=data)
-return response
+    params = {"q": "python language"}
+    response = requests.get("https://httpbin.org/get", params=params)
+    return response
+
+def test_3():
+    data = {
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+    }
+    response = requests.post("https://httpbin.org/post", json=data)
+    return response
 
 def test_4():
-data = {
-"name": "Nada",
-"age": 21
-}
-response = requests.put("https://www.google.com/search",
-json=data)
-return response
+    data = {
+        "name": "Nada",
+        "age": 21
+    }
+    response = requests.put("https://httpbin.org/put", json=data)
+    return response
 
 def test_5():
-data = {
-"age": 22
-}
-response = requests.patch("https://www.google.com/search",
-json=data)
-return response
+    data = {
+        "age": 22
+    }
+    response = requests.patch("https://httpbin.org/patch", json=data)
+    return response
 
 def test_6():
-response = requests.delete("https://www.google.com/search")
-return response
+    response = requests.delete("https://httpbin.org/delete")
+    return response
 
 def test_7():
-response = requests.get("https://www.google.com/search"
-,allow_redirects=True)
-return response.headers.get("Location")
+    response = requests.get("https://httpbin.org/redirect/1", allow_redirects=True)
+    return response.headers.get("Location")
